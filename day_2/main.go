52d4ccfd1fb23841a5135e18e9430852fd226f53
Nicholas Argyle <nick@nicholass-air.lan>
2022-12-04 10:27:59 -0500

    Adds js solutions for day 2"

--- day_2/main.go
+++ day_2/main.go
@@ -1,70 +1,70 @@
-package main
+// package main
 
-import (
-	"bufio"
-	"io"
-	"os"
-)
+// import (
+// 	"bufio"
+// 	"io"
+// 	"os"
+// )
 
-var file string = "input.txt"
+// var file string = "input.txt"
 
-func check(e error) {
-	if e != nil {
-		panic(e)
-	}
-}
+// func check(e error) {
+// 	if e != nil {
+// 		panic(e)
+// 	}
+// }
 
-type FileScanner struct {
-	io.Closer
-	*bufio.Scanner
-}
+// type FileScanner struct {
+// 	io.Closer
+// 	*bufio.Scanner
+// }
 
-func createScanner(file string) *FileScanner {
-	readFile, err := os.Open(file)
-	check(err)
-	fileScanner := bufio.NewScanner(readFile)
-	fileScanner.Split(bufio.ScanLines)
-	return &FileScanner{readFile, fileScanner}
-}
+// func createScanner(file string) *FileScanner {
+// 	readFile, err := os.Open(file)
+// 	check(err)
+// 	fileScanner := bufio.NewScanner(readFile)
+// 	fileScanner.Split(bufio.ScanLines)
+// 	return &FileScanner{readFile, fileScanner}
+// }
 
-func part1() {
-	score := 0
-	scoreMap := map[string]int{
-		"A": 1,
-		"B": 2,
-		"C": 3,
-		"X": 1,
-		"Y": 2,
-		"Z": 3,
-	}
+// func part1() {
+// 	score := 0
+// 	scoreMap := map[string]int{
+// 		"A": 1,
+// 		"B": 2,
+// 		"C": 3,
+// 		"X": 1,
+// 		"Y": 2,
+// 		"Z": 3,
+// 	}
 
-	fileScanner := createScanner(file)
+// 	fileScanner := createScanner(file)
 
-	for fileScanner.Scan() {
-		elf := scoreMap[fileScanner.Text()[0:1]]
-		mine := scoreMap[fileScanner.Text()[2:3]]
-		print(fileScanner.Text()[0:1])
-		print(elf)
-		print(" ")
-		score += mine
-		if mine == elf {
-			print("== ")
-			score += 3
-		}
-		if mine > elf {
-			print("< ")
-			score += 6
-		}
-		print(fileScanner.Text()[2:3])
-		print(mine)
-		print(" = ")
-		print(score)
-		print("\n")
-	}
-	println(score)
-	fileScanner.Close()
-}
+// 	for fileScanner.Scan() {
+// 		elf := scoreMap[fileScanner.Text()[0:1]]
+// 		mine := scoreMap[fileScanner.Text()[2:3]]
+// 		print(fileScanner.Text()[0:1])
+// 		print(elf)
+// 		print(" ")
+// 		score += mine
+// 		if mine == elf {
+// 			print("== ")
+// 			score += 3
+// 		}
+// 		if mine > elf {
+// 			print("< ")
+// 			score += 6
+// 		}
+// 		print(fileScanner.Text()[2:3])
+// 		print(mine)
+// 		print(" = ")
+// 		print(score)
+// 		print("\n")
+// 	}
+// 	println(score)
+// 	fileScanner.Close()
+// }
 
-func main() {
-	part1()
-}
+// func main() {
+// 	part1()
+// }
--- day_2/main.js
+++ day_2/main.js
@@ -1,24 +1,81 @@
 const fs = require('fs');
 
-const map = {
-  "A": 1,
-  "B": 2,
-  "C": 3,
-  "X": 1,
-  "Y": 2,
-  "Z": 3,
-}
-let score = 0;
 const file = fs.readFileSync('input.txt', 'utf-8');
-file.split(/\r?\n/).forEach(line =>  {
-  const elf = map[line[0]];
-  const mine = map[line[2]];
-  score += mine
-  if (mine == elf) {
-    score += 3
+
+const typeRelations = {
+  rock: {
+    value: 1,
+    beats: 'scissors',
+    losesTo: 'paper',
+  },
+  paper: {
+    value: 2,
+    beats: 'rock',
+    losesTo: 'scissors',
+  },
+  scissors: {
+    value: 3,
+    beats: 'paper',
+    losesTo: 'rock',
   }
-  if (mine > elf) {
-    score += 6
-  }
-});
-console.log(score);
+}
+
+const pt2Map = {
+  "X": 'lose',
+  "Y": 'draw',
+  "Z": 'win',
+}
+
+const map = {
+  "A": {value: 1, type: 'rock'},
+  "B": {value: 2, type: 'paper'},
+  "C": {value: 3, type: 'scissors'},
+  "X": {value: 1, type: 'rock'},
+  "Y": {value: 2, type: 'paper'},
+  "Z": {value: 3, type: 'scissors'},
+}
+
+const reverseMap = {
+  "rock": 1,
+  "paper": 2,
+  "scissors": 3,
+}
+
+const part1 = () => {
+  let score = 0;
+  file.split("\n").forEach(line =>  {
+    const elf = map[line.split(" ")[0]];
+    const mine = map[line.split(" ")[1]];
+    score += mine.value
+    if (elf.type === mine.type) {
+      score += 3;
+    }
+    if (typeRelations[mine.type].beats === elf.type) {
+      score += 6;
+    }
+  });
+  console.log(score);
+}
+
+const part2 = () => {
+  let score = 0;
+  file.split("\n").forEach(line =>  {
+    const elf = map[line.split(" ")[0]];
+    const toDo = pt2Map[line.split(" ")[1]];
+    console.log(toDo)
+    if (toDo === 'win') {
+      const elfLosesTo = typeRelations[elf.type].losesTo;
+      score += reverseMap[elfLosesTo] + 6;
+    }
+    if (toDo === 'lose') {
+      const elfBeats = typeRelations[elf.type].beats;
+      score += reverseMap[elfBeats];
+    }
+    if (toDo === 'draw') {
+      score += elf.value + 3;
+    }
+  });
+  console.log(score);
+}
+
+part2();
